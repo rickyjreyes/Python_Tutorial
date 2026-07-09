5a13f99bf2f3c4823fac09857e4b7473c8ee01e9
Richard J. Reyes <reyes.ricky30@gmail.com>
2026-07-08 18:17:41 -0700

    Move hello world lesson into beginner order

---
+++ beginner/01_hello_world.py
@@ -0,0 +1,25 @@
+# 01_hello_world.py
+#
+# First program: print text, write comments, and read keyboard input.
+
+print("Hello World!")
+
+# More practice: make your own print statements.
+print("My name is Ricky!!!!")
+print("My favorite color is blue!!!!")
+print("Video games are cool!!!!")
+print("I love to code!!!!")
+print("This is Python :)")
+
+# This is a single-line comment.
+"""
+This is a multi-line comment.
+The computer ignores comments when the program runs.
+"""
+
+# Practice input.
+user_text = input("Enter input: ")
+print("Output:", user_text)
+
+# Repeat the user's text 10 times.
+print(user_text * 10)
